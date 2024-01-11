f8934ed3122d991732274a2edae56fbf8c879c2d
CHANG MIN PARK <110027004+ChangMinPark2@users.noreply.github.com>
2024-01-11 16:30:25 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,132 @@
+const list = document.getElementById('list');
+const createBtn = document.getElementById('create-btn');
+
+let todos = [{
+    id: new Date().getTime(),
+    text: '밥먹기',
+    complete: false
+}];
+
+createBtn.addEventListener('click', createNewTodo);
+
+function createNewTodo() {
+    //새로운 아이템 객체 생성하기
+    const item = {
+        id: new Date().getTime(),
+        text: '',
+        complete: false
+    }
+
+    //배열 처음에 새로운 아이템을 추가한다.
+    todos.unshift(item); //배열의 첫번째에 엘리먼트를 insert 해준다.
+
+    // 요소 생성하기
+    const {itemEl, inputEl, editBtnEl, removeBtnEl} =
+        createTodoElement(item);
+
+    //리스트 요소 안에 방금 생성한 아이템 요소 추가
+    list.prepend(itemEl)
+
+    inputEl.removeAttribute('disabled');
+
+    inputEl.focus();
+    saveToLocalStorage();
+}
+
+function createTodoElement(item) {
+    const itemEl = document.createElement('div'); //div 요소를 생성하니까 div 넣기
+    itemEl.classList.add('item');
+
+    const checkBoxEl = document.createElement('input');
+    checkBoxEl.type = 'checkBox';
+    checkBoxEl.checked = item.complete;
+
+    if (item.complete) {
+        itemEl.classList.add('complete');
+    }
+
+    const inputEl = document.createElement('input');
+    inputEl.type = 'text';
+    inputEl.value = item.text;
+    inputEl.setAttribute('disabled', '');
+
+    const actionsEl = document.createElement('div');
+    actionsEl.classList.add('actions');
+
+    const editBtnEl = document.createElement('button');
+    editBtnEl.classList.add('material-icons');
+    editBtnEl.innerText = 'edit';
+
+    const removeBtnEl = document.createElement('button');
+    removeBtnEl.classList.add('material-icons', 'remove-btn');
+    removeBtnEl.innerText = 'remove_circles';
+
+    checkBoxEl.addEventListener('change', () => {
+        item.complete = checkBoxEl.checked;
+
+        if (item.complete) {
+            itemEl.classList.add('complete');
+        } else {
+            itemEl.classList.remove('complete');
+        }
+        saveToLocalStorage();
+    })
+
+    inputEl.addEventListener('blur', () => {
+        inputEl.setAttribute('disabled', '');
+        saveToLocalStorage();
+    })
+
+    inputEl.addEventListener('input', () => {
+        item.text = inputEl.value
+    })
+
+    editBtnEl.addEventListener('click', () => {
+        inputEl.removeAttribute('disabled');
+        inputEl.focus();
+    })
+
+    removeBtnEl.addEventListener('click', () => {
+        todos = todos.filter(t => t.id !== item.id);
+
+        itemEl.remove();
+        saveToLocalStorage();
+    })
+
+    actionsEl.append(editBtnEl);
+    actionsEl.append(removeBtnEl);
+
+    itemEl.append(checkBoxEl);
+    itemEl.append(inputEl);
+    itemEl.append(actionsEl);
+
+    return {itemEl, inputEl, editBtnEl, removeBtnEl};
+}
+
+function saveToLocalStorage() {
+    const data = JSON.stringify(todos);
+
+    localStorage.setItem('my_todos', data);
+}
+
+function loadFromLocalStorage() {
+    const data = localStorage.getItem('my_todos');
+
+    if (data) {
+        todos = JSON.parse(data);
+    }
+}
+
+function displayTodos() {
+    loadFromLocalStorage();
+
+    for (let i = 0; i < todos.length; i++) {
+        const item = todos[i];
+            const {itemEl} = createTodoElement(item);
+
+            list.append(itemEl);
+    }
+}
+
+displayTodos();
+
